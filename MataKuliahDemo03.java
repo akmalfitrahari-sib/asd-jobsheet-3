0ef933a00d4f1f73f9a39ba42bc4068f36cc9ec7
akmalfitrahari-sib <akmal.fitrahari@gmail.com>
2026-03-04 16:32:48 +0700

    MatkulDemo v1

---
+++ MataKuliahDemo03.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class MataKuliahDemo03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Matakuliah03[] arrayOfMatakuliah03 = new Matakuliah03[3];
+        String kode, nama, dummy;
+        int sks, jumlahJam;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Masukkan Data MataKuliah ke-" + (i + 1));
+            System.out.print("Kode       : ");
+            kode = sc.nextLine();
+            System.out.print("Nama       : ");
+            nama = sc.nextLine();
+            System.out.print("Sks        : ");
+            dummy = sc.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam : ");
+            dummy = sc.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("---------------------------------");
+
+            arrayOfMatakuliah03[i] = new Matakuliah03 (kode, nama, sks, jumlahJam);
+        }
+    }
+}
