b36cce9a48a7f438837a18af7bab6547af8d9ddc
akmalfitrahari-sib <akmal.fitrahari@gmail.com>
2026-03-04 17:07:00 +0700

    Matkul v3

--- Matakuliah03.java
+++ Matakuliah03.java
@@ -13,4 +13,12 @@ public class Matakuliah03 {
         this.sks = sks;
         this.jumlahJam = jumlahJam;
     }
+
+    public void cetakInfo () {
+        System.out.println("Kode         : " + kode);
+        System.out.println("Nama         : " + nama);
+        System.out.println("Sks          : " + sks);
+        System.out.println("Jumlah Jam   : " + jumlahJam);
+        System.out.println("------------------------------------");
+    }
 }
